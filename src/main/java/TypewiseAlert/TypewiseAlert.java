341ffdab5610e91fdf01d203862bbfa475b5dba3
Nivedhitha <swaminathan.nivedhithaa@in.bosch.com>
2023-06-21 07:52:47 +0530

    Tests added, temp classification simplified

--- src/main/java/TypewiseAlert/TypewiseAlert.java
+++ src/main/java/TypewiseAlert/TypewiseAlert.java
@@ -1,8 +1,7 @@
 package TypewiseAlert;
 
 
-public class TypewiseAlert
-{
+public class TypewiseAlert {
     public enum BreachType {
         NORMAL,
         TOO_LOW,
@@ -11,39 +10,39 @@ public class TypewiseAlert
 
     public enum CoolingType {
 
-        PASSIVE_COOLING(0,35),
-        HI_ACTIVE_COOLING(0,45),
-        MED_ACTIVE_COOLING(0,40);
+        PASSIVE_COOLING(0, 35),
+        HI_ACTIVE_COOLING(0, 45),
+        MED_ACTIVE_COOLING(0, 40);
 
-        private int upperlimit;
-        private int lowerlimit;
-
+        private int coolingTypeLowerLimit;
+        private int coolingTypeUpperLimit;
 
         private CoolingType(int lowerLimit, int upperLimit) {
-            this.lowerlimit = lowerlimit;
-            this.upperlimit = upperlimit;
+            this.coolingTypeLowerLimit = lowerLimit;
+            this.coolingTypeUpperLimit = upperLimit;
 
         }
 
-        public int getLowerlimit() {
-            return this.getLowerlimit();
+        public  int getLowerLimit(CoolingType type) {
+            return type.coolingTypeLowerLimit;
         }
-        public int getUpperLimit() {
-            return this.upperlimit;
+
+        public  int getUpperLimit(CoolingType type) {
+            return type.coolingTypeUpperLimit;
         }
     }
 
-    public enum AlertTarget{
+    public enum AlertTarget {
         TO_CONTROLLER,
         TO_EMAIL
     }
 
-    public  BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
+    public BreachType inferBreach(double value, CoolingType coolingType) {
 
-        if(value > lowerLimit && value < upperLimit){
+        if (value > coolingType.coolingTypeLowerLimit && value < coolingType.coolingTypeUpperLimit) {
             return BreachType.NORMAL;
-        }else{
-            if(value > upperLimit){
+        } else {
+            if (value > coolingType.coolingTypeUpperLimit) {
                 return BreachType.TOO_HIGH;
             }
             return BreachType.TOO_LOW;
@@ -52,50 +51,42 @@ public class TypewiseAlert
 
     }
 
+    public void checkAndAlert(
+            AlertTarget alertTarget, CoolingType coolingType, double temperatureInC) {
+        BreachType breachType = inferBreach(temperatureInC, coolingType);
 
-    public  BreachType classifyTemperatureBreach(
-        CoolingType coolingType, double temperatureInC) {
-       return inferBreach(temperatureInC, coolingType.getLowerlimit(), coolingType.getUpperLimit());
-    }
-
-
-    public  void checkAndAlert(
-       AlertTarget alertTarget, CoolingType type, double temperatureInC) {
-      BreachType breachType = classifyTemperatureBreach(
-       type, temperatureInC
-      );
-
-      switch(alertTarget) {
-        case TO_CONTROLLER:
-          sendToController(breachType);
-          break;
-        case TO_EMAIL:
-          sendToEmail(breachType);
-          break;
-      }
+        switch (alertTarget) {
+            case TO_CONTROLLER:
+                sendToController(breachType);
+                break;
+            case TO_EMAIL:
+                sendToEmail(breachType);
+                break;
+        }
     }
     public  void sendToController(BreachType breachType) {
-      int header = 0xfeed;
-      //System.out.printf("%i : %i\n", header, breachType);
-    }
-    public  void sendToEmail(BreachType breachType) {
-      String recepient = "a.b@c.com";
-      switch(breachType) {
-        case TOO_LOW:
-          System.out.printf("To: %s\n", recepient);
-          System.out.println("Hi, the temperature is too low\n");
-          break;
-        case TOO_HIGH:
-          System.out.printf("To: %s\n", recepient);
-          System.out.println("Hi, the temperature is too high\n");
-          break;
-
-      }
+      String header = "0xfeed";
+      System.out.printf( header + " - " + breachType);
     }
 
-    public static void main (String[] args) {
+    public void sendToEmail(BreachType breachType) {
+        String recepient = "a.b@c.com";
+        switch (breachType) {
+            case TOO_LOW:
+                System.out.printf("To: %s\n", recepient);
+                System.out.println("Hi, the temperature is too low\n");
+                break;
+            case TOO_HIGH:
+                System.out.printf("To: %s\n", recepient);
+                System.out.println("Hi, the temperature is too high\n");
+                break;
+
+        }
+    }
+
+    public static void main(String[] args) {
         TypewiseAlert alert = new TypewiseAlert();
-        alert.checkAndAlert(AlertTarget.TO_EMAIL,CoolingType.HI_ACTIVE_COOLING, 50.1);
+        alert.checkAndAlert(AlertTarget.TO_CONTROLLER, CoolingType.HI_ACTIVE_COOLING, 50.1);
     }
 
 }
--- src/test/java/TypewiseAlert/TypewiseAlertTest.java
+++ src/test/java/TypewiseAlert/TypewiseAlertTest.java
@@ -1,5 +1,6 @@
 package TypewiseAlert;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,9 +8,28 @@ import org.junit.Test;
 public class TypewiseAlertTest 
 {
     @Test
-    public void infersBreachAsPerLimits()
+    public void testinfersLowBreachAsPerLimits()
     {
-      assertTrue(TypewiseAlert.inferBreach(12, 20, 30) ==
+      TypewiseAlert alert = new TypewiseAlert();
+      assertTrue(alert.inferBreach(0, TypewiseAlert.CoolingType.HI_ACTIVE_COOLING) ==
         TypewiseAlert.BreachType.TOO_LOW);
     }
+
+    @Test
+    public void testinfersHighBreachAsPerLimits()
+    {
+        TypewiseAlert alert = new TypewiseAlert();
+        assertTrue(alert.inferBreach(45, TypewiseAlert.CoolingType.MED_ACTIVE_COOLING) ==
+                TypewiseAlert.BreachType.TOO_HIGH);
+    }
+
+    @Test
+    public void testinfersNormalBreachAsPerLimits()
+    {
+        TypewiseAlert alert = new TypewiseAlert();
+        assertTrue(alert.inferBreach(25,  TypewiseAlert.CoolingType.PASSIVE_COOLING) ==
+                TypewiseAlert.BreachType.NORMAL);
+    }
+
+
 }
